59bfa310bd0bdfa1dd23f2178bd46d62e259796a
amrazek <amrazek@csu.fullerton.edu>
2019-10-13 04:05:20 -0700

    added jump physics

--- entities/mario.py
+++ entities/mario.py
@@ -1,23 +1,65 @@
 import math
+from typing import NamedTuple
 import pygame
 from pygame import Vector2
 from .entity import Entity
 from util import mario_str_to_pixel_value as mstpv
 import config
 
-factor = 60.  # mario speeds defined in terms of 60 fps
+frames_to_seconds = 60.  # mario speeds defined in terms of 60 fps
+frames_to_seconds_squared = frames_to_seconds ** 2
 
-min_walk_velocity = factor * mstpv('00130')
-max_walk_velocity = factor * mstpv('01900')
-max_run_velocity = factor * mstpv('02900')
-skid_turnaround_velocity = factor * mstpv('00900')
+# horizontal movement constants
+min_walk_velocity = frames_to_seconds * mstpv('00130')
+max_walk_velocity = frames_to_seconds * mstpv('01900')
+max_run_velocity = frames_to_seconds * mstpv('02900')
+skid_turnaround_velocity = frames_to_seconds * mstpv('00900')
 
-walking_acceleration = factor ** 2 * mstpv('00098')
-running_acceleration = factor ** 2 * mstpv('000E4')
-release_deceleration = factor ** 2 * mstpv('000D0')
-skid_deceleration = factor ** 2 * mstpv('001A0')
+walking_acceleration = frames_to_seconds_squared * mstpv('00098')
+running_acceleration = frames_to_seconds_squared * mstpv('000E4')
+release_deceleration = frames_to_seconds_squared * mstpv('000D0')
+skid_deceleration = frames_to_seconds_squared * mstpv('001A0')
 
 num_frames_hold_speed = 10
+
+# momentum constants
+momentum_velocity_threshold = frames_to_seconds * mstpv('01900')
+momentum_start_jump_threshold = frames_to_seconds * mstpv('01D000')
+
+momentum_forward_low = frames_to_seconds_squared * mstpv('00098')
+momentum_forward_fast = frames_to_seconds_squared * mstpv('000E4')
+
+momentum_backward_fast = frames_to_seconds_squared * mstpv('000E4')  # used when current speed > velocity threshold
+momentum_backward_high_initial_speed = frames_to_seconds_squared * mstpv('000D0')  # low cur speed, high initial
+momentum_backward_low_initial_speed = frames_to_seconds_squared * mstpv('00098')  # low cur speed, low initial
+
+
+# air physics constants
+class _JumpParameters(NamedTuple):
+    initial_speed: float  # horizontal speed threshold
+    initial_velocity_y: float  # mario velocity set to this value when jumping
+    jump_button_gravity: float  # gravity applied at this rate when jump button held
+    gravity: float  # gravity applied at this rate unless jump button held
+
+    @staticmethod
+    def create(i_h_speed, initial_velocity_y, jump_gravity, gravity):
+        return _JumpParameters(
+            frames_to_seconds * mstpv(i_h_speed),
+            frames_to_seconds * mstpv(initial_velocity_y),
+            frames_to_seconds_squared * mstpv(jump_gravity),
+            frames_to_seconds_squared * mstpv(gravity))
+
+
+vertical_physics_parameters = [
+    _JumpParameters.create('01000', '04000', '00200', '00700'),
+    _JumpParameters.create('024FF', '04000', '001E0', '00600'),
+    _JumpParameters.create('FFFFF', '05000', '00280', '00900')
+]
+
+level_entry_vertical_physics = _JumpParameters.create('00000', '00000', '00280', '00280')
+
+air_max_velocity = frames_to_seconds * mstpv('04800')
+air_velocity_when_max_exceeded = frames_to_seconds * mstpv('04000')
 
 
 class Mario(Entity):
@@ -28,13 +70,16 @@ class Mario(Entity):
         self.image = pygame.image.load("images/mario.png").convert()
         self.rect = self.image.get_rect()
 
+        # state values
+        # todo: consider changing from frame counter to delta time, so avoid locking update loop at 1/60 dt
         self.position = Vector2()
         self.velocity = Vector2()
-
-        # todo: rather than frame counter, convert to use elapsed time
         self.run_frame_counter = 0  # has nothing to do with animation, see ** in smb_playerphysics.png
         self.use_skid_deceleration = False
         self.skidding = False
+        self.jump_stats = None
+        self.falling_gravity = level_entry_vertical_physics.gravity
+        self.jumped = False
 
         # temp: set initial position of mario
         self.position.x, self.position.y = 0, config.screen_rect.bottom - self.image.get_height() // 2
@@ -56,14 +101,22 @@ class Mario(Entity):
         else:
             self._handle_horizontal_deceleration(dt)
 
-        # todo: handle vertical movement
+        self._handle_vertical_movement(dt)
 
         # update position based on new velocity calculated by the above
         self.position += self.velocity * dt
 
+        # temp: wraparound screen
         if self.position.x + self.image.get_width() > config.screen_rect.width:
             self.position.x = 0.
             self.velocity.x = 0.
+
+        # temp: floor
+        if self.position.y + self.image.get_height() // 2 > config.screen_rect.height:
+            # jump is over, reset
+            self.position.y = config.screen_rect.height - self.image.get_height() // 2
+            self.velocity.y = 0
+            self.jump_stats = None
 
     def draw(self, screen):
         self.rect.center = self.position
@@ -147,7 +200,59 @@ class Mario(Entity):
             self.use_skid_deceleration = False
             self.velocity.x = 0.
 
+    def _handle_vertical_movement(self, dt):
+        """The main tricky bit with this is remember than it's not just jumping that gets mario into the air:
+        falling off ledges and enemy impact counts too"""
+        # todo: enemy impact physics
+
+        if self.input_state.jump and not self.is_airborne and not self.jumped:
+            # jumping just consists of directly setting vertical velocity;
+            # the actual velocity depends on mario's horizontal speed ...
+            jump_stats = next((js for js in vertical_physics_parameters
+                               if math.fabs(self.velocity.x) < js.initial_speed), None)
+
+            assert jump_stats is not None  # likely means velocity got out of control somehow
+
+            self.velocity.y = -jump_stats.initial_velocity_y
+            self.falling_gravity = jump_stats.gravity
+            self.jumped = True  # flag set to know we should use jump stat gravity, rather than last falling gravity
+
+            # note: the initial horizontal speed when jump began is of interest in mid-air momentum calculations,
+            # so copy jump stats and insert current horizontal velocity into it
+            self.jump_stats = _JumpParameters(math.fabs(self.velocity.x), *jump_stats[1:])
+
+            return
+
+        # update player vertical physics
+        if self.is_airborne:
+            if self.jumped:
+                assert self.jump_stats
+
+                # holding jump alters gravity, so long as mario is moving upwards
+                gravity = self.jump_stats.jump_button_gravity \
+                    if self.input_state.jump and self.velocity.y < 0. else self.jump_stats.gravity
+            else:  # fell off a ledge, use last falling gravity
+                gravity = self.falling_gravity
+
+            self.velocity.y += gravity * dt
+
+            # limit vertical DOWNWARD velocity
+            # for some reason it wraps around, kept for sake of authenticity
+            self.velocity.y = air_velocity_when_max_exceeded if self.velocity.y > air_max_velocity else self.velocity.y
+        else:
+            self.jumped = self.jumped and self.input_state.jump
+
     @property
     def is_running(self):
         """Return true if mario is moving at a running speed"""
         return math.fabs(self.velocity.x) > max_walk_velocity
+
+    @property
+    def is_airborne(self):
+        """Returns true if mario is in the air"""
+        return math.fabs(self.velocity.y) > 0. or \
+            self.position.y < config.screen_rect.height - self.image.get_height() // 2.
+
+        # todo: remove y coordinate when collision is implemented
+        # note to self: consider that velocity.y == 0 at height of arc
+        # which means this is more than just a velocity check
