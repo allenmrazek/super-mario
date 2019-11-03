15560299391747d7590038e913a9a0970b2001c1
amrazek <amrazek@csu.fullerton.edu>
2019-11-03 08:44:15 -0800

    vertical platform complete

--- entities/characters/platform.py
+++ entities/characters/platform.py
@@ -22,12 +22,8 @@ class Platform(LevelEntity):
         from entities.characters.behaviors import Interactive
 
         # need to register a collider in the world for mario to stand on
-        self.collider = Collider.from_entity(self, level.collider_manager, 0)
+        self.collider = Collider.from_entity(self, level.collider_manager, constants.Block)
         level.collider_manager.register(self.collider)
-
-        # this collider used to determine if we hit another platform
-        self.push_collider = Collider.from_entity(self, level.collider_manager, constants.Block)
-        self.push_collider.on_collision = self.on_hit_block
 
         # now another collider to let us know when to drop the platform
         # remember: it assumes unscaled values, while animation has been scaled
@@ -50,32 +46,41 @@ class Platform(LevelEntity):
     def detach_mario(self):
         self.level.mario.glued = False
 
-    def on_hit_block(self, collision):
-        if collision.moved_collider and collision.moved_collider.entity and isinstance(collision.moved_collider.entity, Platform):
-            self._pushed = True
-            print("pushed")
 
     def update(self, dt, view_rect):
+        self.collider.position = self.position
+
         # is mario standing on the platform?
         self.platform_tester.update(dt)
-        self.push_collider.position = self.position
+
 
         mario = self.level.mario
         mario_foot_y = mario.movement.get_foot_position().y
 
         if mario.glued or self._pushed:
-            # mario has been glued onto the platform, we're responsible for moving him (ypos) now
+
             # todo: platform can drag mario into solid blocks, fix
 
             pos = self.position
             pos.y += Platform.FALL_RATE * dt
-            #self.push_collider.try_move(pos, True)
-            #self.position = self.push_collider.position
-            self.position = pos
-            
+
+            for collision in self.collider.try_move(pos, True):
+                if collision.hit_collider and collision.hit_collider.entity:
+                    thing_hit = collision.hit_collider.entity
+
+                    if isinstance(thing_hit, Platform):
+                        # shove it out of the way
+                        thing_hit.position = make_vector(thing_hit.position.x,
+                                                         thing_hit.position.y + Platform.FALL_RATE * dt)
+                    else:
+                        self.collider.position = self.position  # todo: better way to handle this? hit a block
+
+            self.position = self.collider.position
+
             # want to align mario's feet with our top if he's glued onto the platform
             if mario.glued:
-                mario.movement.set_foot_y_coord(pos.y)
+                # mario has been glued onto the platform, we're responsible for moving him (ypos) now
+                mario.movement.set_foot_y_coord(self.position.y)
 
     def draw(self, screen, view_rect):
         screen.blit(self.animation.image, world_to_screen(self.position, view_rect))
